b8c96e8a1c59886d7ae38748dc0f37ffcb6e2117
AndrewValentini <avalentini@carthage.edu>
2022-06-28 13:38:16 -0500

    replaced original code with three arrays

--- scripts/dependency-helper.py
+++ scripts/dependency-helper.py
@@ -15,105 +15,46 @@ def basic():
         stderr=subprocess.STDOUT)
     except subprocess.SubprocessError:
         print(f"{bcolors.OKCYAN}mpg-foss: could not update pip...{bcolors.ENDC}")
-    try:
-        import wheel
-    except ImportError:
-        print(f"{bcolors.OKCYAN}mpg-foss: wheel not found...{bcolors.ENDC}")
-        print(f"{bcolors.OKCYAN}mpg-foss: installing wheel...{bcolors.ENDC}")
-        subprocess.check_call([sys.executable, "-m", "pip", "install", 'wheel'])
-    try:
-        import halo
-    except ImportError:
-        print(f"{bcolors.OKCYAN}mpg-foss: halo not found...{bcolors.ENDC}")
-        print(f"{bcolors.OKCYAN}mpg-foss: installing halo...{bcolors.ENDC}")
-        subprocess.check_call([sys.executable, "-m", "pip", "install", 'halo'])
+
+    installations = ['wheel', 'halo']
+
+    for installation in installations:
+        try:
+            __import__ (installation)
+        except ImportError:
+            print(f"{bcolors.OKCYAN}mpg-foss: {installation} not found...{bcolors.ENDC}")
+            print(f"{bcolors.OKCYAN}mpg-foss: installing {installation}...{bcolors.ENDC}")
+            subprocess.check_call([sys.executable, "-m", "pip", "install", installation])
+    
     pass
 
 def advanced():
-    try:
-        import struct
-    except ImportError:
-        print(f"{bcolors.OKCYAN}mpg-foss: struct not found...{bcolors.ENDC}")
-        print(f"{bcolors.OKCYAN}mpg-foss: installing struct...{bcolors.ENDC}")
-        subprocess.check_call([sys.executable, "-m", "pip", "install", 'struct'])
-    try:
-        import usb.core
-    except ImportError:
-        print(f"{bcolors.OKCYAN}mpg-foss: pyusb not found...{bcolors.ENDC}")
-        print(f"{bcolors.OKCYAN}mpg-foss: installing pyusb...{bcolors.ENDC}")
-        subprocess.check_call([sys.executable, "-m", "pip", "install", 'pyusb'])
-    try:
-        import pandas
-    except ImportError:
-        print(f"{bcolors.OKCYAN}mpg-foss: pandas not found...{bcolors.ENDC}")
-        print(f"{bcolors.OKCYAN}mpg-foss: installing pandas...{bcolors.ENDC}")
-        subprocess.check_call([sys.executable, "-m", "pip", "install", 'pandas'])
-    try:
-        import matplotlib
-    except ImportError:
-        print(f"{bcolors.OKCYAN}mpg-foss: matplotlib not found...{bcolors.ENDC}")
-        print(f"{bcolors.OKCYAN}mpg-foss: installing matplotlib...{bcolors.ENDC}")
-        subprocess.check_call([sys.executable, "-m", "pip", "install", 'matplotlib'])
-    try:
-        import statsmodels
-    except ImportError:
-        print(f"{bcolors.OKCYAN}mpg-foss: statsmodels not found...{bcolors.ENDC}")
-        print(f"{bcolors.OKCYAN}mpg-foss: installing statsmodels...{bcolors.ENDC}")
-        subprocess.check_call([sys.executable, "-m", "pip", "install", 'statsmodels']) 
-    """
-    try:
-        import ttkbootstrap
-    except ImportError:
-        print(f"{bcolors.OKCYAN}mpg-foss: ttkbootstrap not found...{bcolors.ENDC}")
-        print(f"{bcolors.OKCYAN}mpg-foss: installing ttkbootstrap...{bcolors.ENDC}")
-        subprocess.check_call([sys.executable, "-m", "pip", "install", 'ttkbootstrap'])
-    """
+
+    dependencies = ['struct', 'usb.core', 'pandas', 'matplotlib', 'statsmodels', 'ttkbootstrap']
+
+    for dependency in dependencies:
+        try:
+            
+            __import__ (dependency)
+        except ImportError:
+            print(f"{bcolors.OKCYAN}mpg-foss: {dependency} not found...{bcolors.ENDC}")
+            print(f"{bcolors.OKCYAN}mpg-foss: installing {dependency}...{bcolors.ENDC}")
+            subprocess.check_call([sys.executable, "-m", "pip", "install", dependency])
+
     pass
 
 def test():
     print(f"{bcolors.HEADER}mpg-foss: Verifying...{bcolors.ENDC}")
-    try:
-        import wheel
-    except ImportError:
-        fail = True
-        print(f"{bcolors.FAIL}mpg-foss: Could not import wheel. Try manually installing it. {bcolors.ENDC}")
-    try:
-        import halo
-    except ImportError:
-        fail = True
-        print(f"{bcolors.FAIL}mpg-foss: Could not import halo. Try manually installing it. {bcolors.ENDC}")
-    try:
-        import struct
-    except ImportError:
-        fail = True
-        print(f"{bcolors.FAIL}mpg-foss: Could not import struct. Try manually installing it. {bcolors.ENDC}")
-    try:
-        import usb.core
-    except ImportError:
-        fail = True
-        print(f"{bcolors.FAIL}mpg-foss: Could not import pyusb. Try manually installing it. {bcolors.ENDC}")
-    try:
-        import pandas
-    except ImportError:
-        fail = True
-        print(f"{bcolors.FAIL}mpg-foss: Could not import pandas. Try manually installing it. {bcolors.ENDC}")
-    try:
-        import matplotlib
-    except ImportError:
-        fail = True
-        print(f"{bcolors.FAIL}mpg-foss: Could not import matplotlib. Try manually installing it. {bcolors.ENDC}")
-    try:
-        import statsmodels
-    except ImportError:
-        fail = True
-        print(f"{bcolors.FAIL}mpg-foss: Could not import statsmodels. Try manually installing it. {bcolors.ENDC}")
-    """
-    try:
-        import ttkbootstrap
-    except ImportError:
-        fail = True
-        print(f"{bcolors.FAIL}mpg-foss: Could not import ttkbootstrap. Try manually installing it. {bcolors.ENDC}")
-    """
+
+    graphics = ['halo', 'struct', 'usb.core', 'pandas', 'matplotlib', 'statsmodels', 'ttkbootstrap']
+    
+    for graphic in graphics:
+        try:
+            __import__ (graphic)
+        except ImportError:
+            fail = True
+            print(f"{bcolors.FAIL}mpg-foss: Could not import {graphic}. Try manually installing it. {bcolors.ENDC}")
+    
     pass
 
 try:
