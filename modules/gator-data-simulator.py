44e334770a88516ac83b9f59dca4697bae0057ad
Caleb A. Collar <49846301+Mindstormer-0@users.noreply.github.com>
2022-07-23 20:06:13 -0500

    Removed unused imports.

--- modules/gator-data-simulator.py
+++ modules/gator-data-simulator.py
@@ -1,16 +1,6 @@
 #Written by Caleb C. & Andrew V. in 2022 for Carthage Space Sciences | WSGC | NASA
-#Simulates gator data for the purposes of testing.
+#Simulates gator data at the hardware (USB) level for the purposes of testing.
 from formatmodule import bcolors, bsymbols
-import struct
-import pandas as pd 
-import numpy as np
-import matplotlib.pyplot as plt
-import matplotlib as mpl
-import statsmodels.api as sm 
-from scipy.fft import fft
-
-#TODO: Check if noise is being generated correctly.
-#TODO: Covert noise to data in gator format and transmit.
 
 from halo import Halo
 fail = False
