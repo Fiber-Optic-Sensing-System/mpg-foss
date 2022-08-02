de2369eef6fd2f88a409609032f7f2435e42c145
AndrewValentini <avalentini@carthage.edu>
2022-08-02 10:02:01 -0500

    Began adding ability to plot

---
+++ modules/graphical-tools.py
@@ -0,0 +1,9 @@
+""""
+Written by Caleb C. & Andrew Valentini in 2022 for Carthage Space Sciences | WSGC | NASA
+Graphs the strain data as an FFT plot
+"""
+import matplotlib.pyplot as plt
+import numpy as np
+
+plt.figure(figsize=(10,10))
+plt.plot()
