1014103dfaaab9c35f777c9e80788eabdcfb0872
Caleb A. Collar <49846301+Mindstormer-0@users.noreply.github.com>
2023-03-31 14:37:17 -0500

    feat: Interpreter version check at start

--- foss.py
+++ foss.py
@@ -1,51 +1,10 @@
 #!/usr/bin/env python
-#Written by Caleb C. in 2022 for Carthage Space Sciences | WSGC | NASA
-import argparse
+#Written by Caleb C. in 2023 for Carthage Space Sciences | WSGC | NASA
+import sys
+#Check Interpreter Version
+if sys.version_info < (3, 10):
+    print("\033[91m\U00002717 \033[91m\033[1mmpg-foss: Python version 3.10 or newer is required to run this program!\033[0m")
+    print(f"\033[94m\U00002139 \033[94m\033[1mmpg-foss: Your current Python version is {sys.version_info.major}.{sys.version_info.minor}.{sys.version_info.micro}\033[0m")
+    exit(1)
 import subprocess
-from modules.formatmodule import bcolors, bsymbols, prints
-
-def handle_args():
-    parser = argparse.ArgumentParser(description='Run foss.py help for more information. Run foss.py deps to install dependencies.')
-    parser.add_argument('argument', type=str)
-    args = parser.parse_args()
-    return args
-
-def switch(args):
-    match args.argument:
-        case "deps" | "dependencies" | "update":
-            print(f"{bcolors.OKBLUE}{bsymbols.info} {bcolors.OKBLUE}{bcolors.BOLD}mpg-foss: Starting dependency check tool...{bcolors.ENDC}")
-            subprocess.call("python ./modules/dependency-helper.py", shell=True)
-            return
-        case "data" | "d" | "collect":
-            print(f"{bcolors.OKBLUE}{bsymbols.info} {bcolors.OKBLUE}{bcolors.BOLD}mpg-foss: Starting gator-data collection tool...{bcolors.ENDC}")
-            subprocess.call("python ./modules/gator-data-to-csv.py", shell=True)
-            return
-        case "sim":
-            print(f"{bcolors.OKBLUE}{bsymbols.info} {bcolors.OKBLUE}{bcolors.BOLD}mpg-foss: Starting gator-data simulator...{bcolors.ENDC}")
-            subprocess.call("python ./modules/gator-data-simulator.py", shell=True)
-            return
-        case "clean" | "cleanup" | "remove":
-            print(f"{bcolors.OKBLUE}{bsymbols.info} {bcolors.OKBLUE}{bcolors.BOLD}mpg-foss: Clean output directory...{bcolors.ENDC}")
-            subprocess.call("python ./modules/cleanup.py", shell=True)
-            return
-        case "wisdom" | "zen":
-            print(f"{bcolors.OKBLUE}{bsymbols.info} {bcolors.OKBLUE}{bcolors.BOLD}mpg-foss: Providing you with design ethic...{bcolors.ENDC}")
-            subprocess.call("python ./modules/design-wisdom.py", shell=True)
-            return
-        case _:
-            #launch help
-            print(f"{bcolors.OKBLUE}{bsymbols.info} {bcolors.FAIL}{bcolors.BOLD}mpg-foss: Unknown argument...{bcolors.ENDC}")
-            print(f"{bcolors.OKBLUE}{bsymbols.info} {bcolors.OKGREEN}mpg-foss: Available commands:{bcolors.ENDC}")
-            print(f"    deps{bcolors.ENDC}")
-            print(f"    data{bcolors.ENDC}")
-            print(f"    clean{bcolors.ENDC}")
-            print(f"    sim{bcolors.ENDC}")
-            print(f"    wisdom{bcolors.ENDC}")
-            return
-
-#Run
-p = prints()
-args = handle_args()
-p.title()
-switch(args)
-
+subprocess.call(f"python foss_start.py {sys.argv[1]}", shell=True)
